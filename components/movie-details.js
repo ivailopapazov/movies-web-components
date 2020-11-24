19cc6ff32deaddab0bb1fdaf7606bf013add6a69
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2020-11-24 22:07:16 +0200

    Add like functionallity

--- components/movie-details.js
+++ components/movie-details.js
@@ -1,5 +1,12 @@
 import { html, render } from 'https://unpkg.com/lit-html?module';
-import { getOneMovie } from '../services/movieServices.js';
+import { getOneMovie, likeMovie } from '../services/movieServices.js';
+import { getUserData } from '../services/authServices.js';
+
+const hasLiked = (likes, email) => {
+    return Object
+        .values(likes)
+        .some(like => like.creator == email);
+}
 
 const template = (ctx) => html`
     <div class="container">
@@ -12,21 +19,42 @@ const template = (ctx) => html`
             <div class="col-md-4 text-center">
                 <h3 class="my-3 ">Movie Description</h3>
                 <p>"${ctx.description}"</p>
-                <a class="btn btn-danger" href="#">Delete</a>
-                <a class="btn btn-warning" href="#">Edit</a>
-                <a class="btn btn-primary" href="#">Like</a>
-                <span class="enrolled-span">Liked 1</span>
+                ${ctx.creator == ctx.user.email
+                    ? html`
+                        <a class="btn btn-danger" href="#">Delete</a>
+                        <a class="btn btn-warning" href="#">Edit</a>
+                    `
+                    : html `
+                        ${hasLiked(ctx.likes, ctx.user.email)
+                            ? html`<span class="enrolled-span">Liked ${Object.keys(ctx.likes).length}</span>`
+                            : html`<a class="btn btn-primary" @click=${ctx.onLike}>Like</a>`
+                        }
+                    `
+                }
             </div>
         </div>
     </div>
 `;
 
 class MovieDetails extends HTMLElement {
+    constructor() {
+        super();
+
+        this.user = getUserData();
+    }
+    
     connectedCallback() {
         getOneMovie(this.location.params.id)
             .then(data => {
                 Object.assign(this, data);
                 this.render();
+            });
+    }
+
+    onLike(e) {
+        likeMovie(this.location.params.id, this.user.email)
+            .then(data => {
+                this.requestUpdate();
             });
     }
 
--- services/movieServices.js
+++ services/movieServices.js
@@ -13,8 +13,8 @@ export const getAllMovies = async (searchText) => {
 
 export const getOneMovie = async (id) => {
     let res = await request(`${databaseUrl}/movies/${id}.json`, 'GET');
-    
-    return res;
+
+    return Object(res, {id});
     // let { email } = authService.getData();
 
     // let likes = Object.values(res.likes || {});
@@ -22,3 +22,9 @@ export const getOneMovie = async (id) => {
 
     // return Object.assign(res, {isOwn: res.creator == email, alreadyLiked, likes: likes.length});
 };
+
+export const likeMovie = async (id, creator) => {
+    let res = await request(`${databaseUrl}/movies/${id}/likes.json`, 'POST', {creator});
+
+    return res;
+}
