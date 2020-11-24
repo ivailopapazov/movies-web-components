5332b0fc175b293905c2bf7e0754cb7ef318169e
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2020-11-24 19:11:05 +0200

    Add simple components to render

---
+++ components/home.js
@@ -0,0 +1,7 @@
+class Home extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = 'In Home Component'
+    }
+};
+
+export default Home;
---
+++ components/register.js
@@ -0,0 +1,7 @@
+class Register extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = 'In Register Component'
+    }
+};
+
+export default Register;
