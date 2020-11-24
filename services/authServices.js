3375cc17a2c894c8e65f01db4f34b43c87627fd4
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2020-11-24 20:20:21 +0200

    Successful login

--- services/authServices.js
+++ services/authServices.js
@@ -1,11 +1,25 @@
 const apiKey = 'AIzaSyDraSd0Lxkzx1AIJBhMKgaM-1AqkV_6QWw';
-const databaseUrl = `https://movies-4a050.firebaseio.com`;
-const registerUrl = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${apiKey}`;
+
+const api = {
+    register: `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${apiKey}`,
+    login:`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${apiKey}`,
+};
 
 import { request } from './requestServices.js';
 
 export const register = async (email, password) => {
-    let res = await request(registerUrl, 'POST', {
+    let res = await request(api.register, 'POST', {
+        email,
+        password,
+    });
+
+    localStorage.setItem('auth', JSON.stringify(res));
+
+    return res;
+};
+
+export const login = async (email, password) => {
+    let res = await request(api.login, 'POST', {
         email,
         password,
     });
